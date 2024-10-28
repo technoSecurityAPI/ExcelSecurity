e1168691ccb5d13af73d176a131c8f00e6a48dcd
technoSecurityAPI <chirag.nayak@technomak.com>
2024-10-28 17:16:11 +0400

    refer to the code from the internet

--- office_access/deployment.py
+++ office_access/deployment.py
@@ -1,34 +1,35 @@
 import os
-from .settings import *
-from .settings import BASE_DIR
+from .settings import *  # Import all base settings
 
-SECRET_KEY=os.environ['SECRET']
-ALLOWED_HOSTS=[os.environ['WEBSITE_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS=['https://'+os.environ['WEBSITE_HOSTNAME']]
-DEBUG=True
+# Use environment variables for sensitive values
+SECRET_KEY = os.environ['SECRET']
+DEBUG = False  # Turn off debug mode in production
 
+# Configure allowed hosts and CSRF trusted origins
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 
+# Add Whitenoise for serving static files in production
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Whitenoise for static files
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
 ]
 
+# Configure Whitenoise static file settings
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+# PostgreSQL configuration using the connection string
+connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+parameters = dict(pair.split('=') for pair in connection_string.split(';') if pair)
 
-STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
-
-connection_string=os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-parameters={pair.split('='):pair.split('=')[1] for pair in connection_string.split(' ')}
-
-DATABASES={
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': parameters['dbname'],
